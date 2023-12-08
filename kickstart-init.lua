483b180f8b3b8b8b9eac3066da9601f0f6f9b973
Bhavesh164 <bhaveshverma333@gmail.com>
2023-12-08 15:25:31 +0530

    Update kickstart-init.lua find files based on git conditions
    find files based on git conditions

--- kickstart-init.lua
+++ kickstart-init.lua
@@ -344,7 +344,17 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
-vim.keymap.set('n', '<leader>ff', require('telescope.builtin').git_files, { desc = '[S]earch [F]iles' })
+
+local function chooseFileFinder()
+    local git_dir = vim.fn.finddir('.git', '.;')
+    if git_dir ~= '' then
+        return require('telescope.builtin').git_files
+    else
+        return require('telescope.builtin').find_files
+    end
+end
+
+vim.keymap.set('n', '<leader>ff', chooseFileFinder(), { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>o', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
