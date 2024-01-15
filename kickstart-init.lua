1a9d27ca04d5c48bf14b2d7c58a85d1677df6d03
Bhavesh164 <bhaveshverma333@gmail.com>
2024-01-15 22:53:41 +0530

    Update kickstart-init.lua

--- kickstart-init.lua
+++ kickstart-init.lua
@@ -229,6 +229,18 @@ require('lazy').setup({
     config = function()
       local wilder = require('wilder')
       wilder.setup({modes = {':', '/', '?'}})
+      wilder.set_option('pipeline', {
+	  wilder.branch(
+		wilder.cmdline_pipeline({
+		  -- sets the language to use, 'vim' and 'python' are supported
+		  language = 'vim',
+		  -- 0 turns off fuzzy matching
+		  -- 1 turns on fuzzy matching
+		  -- 2 partial fuzzy matching (match does not have to begin with the same first letter)
+		  fuzzy = 1,
+		})
+  	),
+	})
     end,
   },
   {
